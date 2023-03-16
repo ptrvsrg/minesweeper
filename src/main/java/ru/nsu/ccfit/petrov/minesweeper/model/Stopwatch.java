9f5284c5038e3aafd3a70f5312020460f8a3dc35
ptrvsrg <s.petrov1@g.nsu.ru>
2023-03-16 22:08:36 +0700

    Add stopwatch

---
+++ src/main/java/ru/nsu/ccfit/petrov/minesweeper/model/Stopwatch.java
@@ -0,0 +1,45 @@
+package ru.nsu.ccfit.petrov.minesweeper.model;
+
+import java.util.TimerTask;
+
+
+public class Stopwatch
+{
+    private int second = 0;
+    private boolean isRunning = false;
+    private java.util.Timer timer;
+
+    private class StopwatchTask
+        extends TimerTask {
+        @Override
+        public void run() {
+            ++second;
+        }
+    }
+
+    public void run() {
+        if (isRunning)
+            return;
+
+        isRunning = true;
+        timer = new java.util.Timer();
+        timer.scheduleAtFixedRate(new StopwatchTask(), 0, 1000);
+    }
+
+    public void stop() {
+        if (!isRunning)
+            return;
+
+        isRunning = false;
+        timer.cancel();
+        timer.purge();
+    }
+
+    public void clear() {
+        second = 0;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+}
