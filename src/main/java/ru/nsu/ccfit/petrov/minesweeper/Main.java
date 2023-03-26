c0f84ba22d72ac7226f95e7636dffb1421ab97f6
ptrvsrg <s.petrov1@g.nsu.ru>
2023-03-26 18:14:33 +0700

    Add Main class

---
+++ src/main/java/ru/nsu/ccfit/petrov/minesweeper/Main.java
@@ -0,0 +1,35 @@
+package ru.nsu.ccfit.petrov.minesweeper;
+
+import org.apache.commons.cli.ParseException;
+import ru.nsu.ccfit.petrov.minesweeper.clparser.CLParser;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CLParser clParser = new CLParser();
+
+        try {
+            if (!clParser.parse(args))
+                return;
+        }
+        catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        String uiMode = clParser.getUIMode();
+        if (uiMode == null) {
+            uiMode = "GUI";
+        }
+
+        switch (uiMode) {
+            case "GUI":
+                new ru.nsu.ccfit.petrov.minesweeper.view.gui.StartMenu();
+                break;
+            case "Text":
+                new ru.nsu.ccfit.petrov.minesweeper.view.text.StartMenu();
+                break;
+            default:
+                throw new IllegalArgumentException("No such UI mode found\n");
+        }
+    }
+}
