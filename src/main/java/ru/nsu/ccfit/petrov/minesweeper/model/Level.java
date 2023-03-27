ba45181b25a62714edc8013286084104885f4ca0
ptrvsrg <s.petrov1@g.nsu.ru>
2023-03-27 18:20:47 +0700

    Add javadoc

--- src/main/java/ru/nsu/ccfit/petrov/minesweeper/model/Level.java
+++ src/main/java/ru/nsu/ccfit/petrov/minesweeper/model/Level.java
@@ -1,7 +1,21 @@
 package ru.nsu.ccfit.petrov.minesweeper.model;
 
+/**
+ * The type {@code Level} is enum for definition game level.
+ *
+ * @author ptrvsrg
+ */
 public enum Level {
+    /**
+     * Beginner level.
+     */
     BEGINNER,
+    /**
+     * Intermediate level.
+     */
     INTERMEDIATE,
+    /**
+     * Expert level.
+     */
     EXPERT
 }
