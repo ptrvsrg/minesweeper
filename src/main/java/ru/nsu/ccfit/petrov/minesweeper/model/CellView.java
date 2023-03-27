0fec85dbc85746c565fd6842b9177fc697e8778b
ptrvsrg <s.petrov1@g.nsu.ru>
2023-03-27 17:57:07 +0700

    Add javadoc

--- src/main/java/ru/nsu/ccfit/petrov/minesweeper/model/CellView.java
+++ src/main/java/ru/nsu/ccfit/petrov/minesweeper/model/CellView.java
@@ -1,5 +1,21 @@
 package ru.nsu.ccfit.petrov.minesweeper.model;
 
+/**
+ * The type {@code CellView} is enum for to define cell view mode.
+ *
+ * @author ptrvsrg
+ */
 public enum CellView {
-    OPENED, CLOSED, MARKED
+    /**
+     * Opened cell.
+     */
+    OPENED,
+    /**
+     * Closed cell.
+     */
+    CLOSED,
+    /**
+     * Marked cell.
+     */
+    MARKED
 }
