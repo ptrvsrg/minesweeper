b19dbd1a0efbfa2f3f8f59a441498147214f75e0
ptrvsrg <s.petrov1@g.nsu.ru>
2023-03-27 18:45:32 +0700

    Add javadoc

--- src/main/java/ru/nsu/ccfit/petrov/minesweeper/view/PlayerStatus.java
+++ src/main/java/ru/nsu/ccfit/petrov/minesweeper/view/PlayerStatus.java
@@ -2,6 +2,8 @@ package ru.nsu.ccfit.petrov.minesweeper.view;
 
 /**
  * The type {@code PlayerStatus} is enum for definition result of game.
+ *
+ * @author ptrvsrg
  */
 public enum PlayerStatus {
     /**
