bc0d7caaea8dc4c9a37614292cb0926e4e9c6c27
ptrvsrg <s.petrov1@g.nsu.ru>
2023-03-27 18:18:49 +0700

    Add javadoc

--- src/main/java/ru/nsu/ccfit/petrov/minesweeper/model/Field.java
+++ src/main/java/ru/nsu/ccfit/petrov/minesweeper/model/Field.java
@@ -5,6 +5,11 @@ import java.util.Random;
 import java.util.Set;
 import lombok.Getter;
 
+/**
+ * The type {@code Field} is class for work with field.
+ *
+ * @author ptrvsrg
+ */
 public class Field {
     private static final int MINE = -1;
     private static final Random randomGenerator = new Random();
@@ -16,8 +21,16 @@ public class Field {
     private final int mineCount;
     private final CellView[][] cellViewMatrix;
     private final byte[][] mineMatrix;  // -1 is mine
-    // another number is the number of mines around
+                                        // another number is the number of mines around
 
+    /**
+     * Instantiates a new Field.
+     *
+     * @param height    the height
+     * @param width     the width
+     * @param mineCount the mine count
+     * @throws IllegalArgumentException if height, width or mine count have negative value
+     * */
     public Field(int height, int width, int mineCount) {
         if (height < 0 || width < 0) {
             throw new IllegalArgumentException("Incorrect size");
@@ -89,31 +102,69 @@ public class Field {
         return nums;
     }
 
+    /**
+     * Gets view of cell with coordinates (x, y).
+     *
+     * @param y the y coordinates
+     * @param x the x coordinates
+     * @return the cell view
+     * @throws IllegalArgumentException if coordinates are outside field
+     */
+    public CellView getCellView(int y, int x) {
+        checkCoordinates(y, x);
+        return cellViewMatrix[y][x];
+    }
+
+    /**
+     * Sets view of cell with coordinates (x, y).
+     *
+     * @param y the y coordinates
+     * @param x the x coordinates
+     * @throws IllegalArgumentException if coordinates are outside field
+     */
+    public void setCellView(int y, int x, CellView cellView) {
+        checkCoordinates(y, x);
+        cellViewMatrix[y][x] = cellView;
+    }
+
+    /**
+     * Gets mine count around cell with coordinates (x, y).
+     *
+     * @param y the y coordinates
+     * @param x the x coordinates
+     * @throws IllegalArgumentException if coordinates are outside field
+     */
+    public byte getMineCountAround(int y, int x) {
+        checkCoordinates(y, x);
+        return mineMatrix[y][x];
+    }
+
     private void checkCoordinates(int y, int x) {
         if (y < 0 || y >= height || x < 0 || x >= width) {
             throw new IllegalArgumentException("Incorrect coordinates");
         }
     }
 
-    public CellView getCellView(int y, int x) {
-        checkCoordinates(y, x);
-        return cellViewMatrix[y][x];
-    }
-
-    public byte getMineCountAround(int y, int x) {
-        checkCoordinates(y, x);
-        return mineMatrix[y][x];
-    }
-
-    public void setCellView(int y, int x, CellView cellView) {
-        checkCoordinates(y, x);
-        cellViewMatrix[y][x] = cellView;
-    }
-
+    /**
+     * Is cell with coordinates (x, y) mine?
+     *
+     * @param y the y coordinates
+     * @param x the x coordinates
+     * @return {@code true} - cell with coordinates (x, y) is mine,
+     * {@code false} - cell with coordinates (x, y) is not mine
+     */
     public boolean isMine(int y, int x) {
         return getMineCountAround(y, x) == MINE;
     }
 
+    /**
+     * Are there mines around cell with coordinates (x, y)?
+     *
+     * @param y the y coordinates
+     * @param x the x coordinates
+     * @return {@code true} - there are mines around,
+     * {@code false} - there are no mines around
+     */
     public boolean areThereMinesAround(int y, int x) {
         return getMineCountAround(y, x) > 0;
     }
